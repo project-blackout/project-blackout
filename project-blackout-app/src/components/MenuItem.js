2d05914dd3681b014b42a2b804955ea1f64efb3f
Stanley Omotuyole <somotuyole@box.com>
2022-05-19 14:30:36 -0700

    adds images and styling

--- project-blackout-app/src/components/MenuItem.js
+++ project-blackout-app/src/components/MenuItem.js
@@ -4,15 +4,13 @@ export default function MenuItem(props) {
   return (
         <div className = "menu-item" {...props}>
             <div className = "item-image-container">
-                <div>
-                    <img src = "./Oval.png" alt = ""/>
+                <div className='item-image-div'>
+                    <img className='item-image' src = {`./images/Oval(${props.image}).png`} alt = ""/>
                 </div>
             </div>
-            <div className = "item-name-container">
-                <div>    
-                <h2>{props.title}</h2>
-                <p>{props.description}</p>
-                </div>
+            <div className = "item-name-container">  
+                <span className='item-title'>{props.title}</span>
+                <span className='item-description'>{props.description}</span>
             </div>
         </div>
 )};
