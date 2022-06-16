4c7611de955be527a2299d0d8bf49e935edfa62c
Shafee Rushdan <srushdan@box.com>
2022-06-16 13:21:04 -0700

    Item page cont.

--- project-blackout-app/src/components/Item.js
+++ project-blackout-app/src/components/Item.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React from "react";
 
 function Item() {
   return (
     <div>
-     <h1>This is the item page</h1>
-     
-     
+      <div class="item-container">
+        <h1>Details</h1>
+
+        <img
+          height="100%"
+          width="100%"
+          src="https://media.newyorker.com/photos/5fe265a5a9dbd8900622a7c9/master/pass/210104_r37671.jpg"
+          alt="Italian Trulli"
+        />
+
+        <div class="item-card">
+          <h1>Black Guys Ribs</h1>
+
+          <h2>Website: Facebook.com/blkgui.com</h2>
+
+          <h3>Email</h3>
+
+          <h4>blackguy@gmail.com</h4>
+
+          <h3>Address</h3>
+
+          <h4>915 Malcolm X Blvd</h4>
+
+          <h3>Social URL</h3>
+
+          <h4>Facebook.com/blkgui***</h4>
+
+
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Item;
